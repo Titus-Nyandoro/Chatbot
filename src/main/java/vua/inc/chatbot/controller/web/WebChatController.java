d537aece3b624cc6afec8eefeb1be0b66c6784d1
kapadokiatitus <ktnyandoch@gmail.com>
2024-04-18 13:55:30 +0300

    feat: client web interface

---
+++ src/main/java/vua/inc/chatbot/controller/web/WebChatController.java
@@ -0,0 +1,10 @@
+package vua.inc.chatbot.controller.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class WebChatController {
+
+}
